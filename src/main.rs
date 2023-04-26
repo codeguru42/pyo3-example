fc6154756a7224b9b2e943a7f79240df6dd55a2f
Code-Apprentice <codeguru42@gmail.com>
2023-04-26 12:04:52 -0600

    Example with test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use pyo3::exceptions::PyValueError;
+use pyo3::prelude::*;
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[pyclass]
+#[derive(PartialEq, Debug)]
+pub struct Example {
+    num: i32,
+}
+
+#[pymethods]
+impl Example {
+    #[new]
+    fn new(num: i32) -> PyResult<Self> {
+        return if num > 0 {
+            Ok(Self { num })
+        } else {
+            Err(PyValueError::new_err("num must be greater than 0"))
+        };
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new() {
+        let result = Example::new(42).unwrap();
+        let expected = Example { num: 42 };
+        assert_eq!(expected, result);
+    }
 }
